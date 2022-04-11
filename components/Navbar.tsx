08d7b77efa473d9a6c7d19818771bb864992041a
qazal <ghazal.nzhrzr@gmail.com>
2022-04-11 15:27:54 +0100

    svg logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Flex } from "@chakra-ui/react";
 import { DarkModeSwitch } from "./DarkModeSwitch";
+import { Logo } from "./Logo";
 
 export const Navbar = () => {
   return (
     <Flex w="100%" h="100%" bg="bg1">
+      <Logo />
       <DarkModeSwitch />
     </Flex>
   );
